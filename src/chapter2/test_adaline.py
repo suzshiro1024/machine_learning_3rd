1f6fcb1e4dafa76af47191dc617a201a4374fe9f
suzshiro1024 <tix131423113210@gmail.com>
2023-03-27 00:37:24 +0900

    2-5-1

---
+++ src/chapter2/test_adaline.py
@@ -0,0 +1,31 @@
+from adaline import AdalineGD
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("../../data/iris.data", header=None)
+
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == "Iris-setosa", -1, 1)
+
+    X = df.iloc[0:100, [0, 2]].values
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+    ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+
+    ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker="o")
+    ax[0].set_xlabel("Epochs")
+    ax[0].set_ylabel("log(Sum-squared-error)")
+    ax[0].set_title("Adalines - Learning rate 0.01")
+
+    ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+
+    ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker="o")
+    ax[1].set_xlabel("Epochs")
+    ax[1].set_ylabel("Sum-squared-error")
+    ax[1].set_title("Adalines - Learning rate 0.01")
+
+    plt.savefig("../../figure/adaline.png")
