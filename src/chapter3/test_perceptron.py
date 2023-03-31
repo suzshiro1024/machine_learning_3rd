32b1942e4581dd8e627aec985b3cfdcdd13b0e56
suzshiro1024 <tix131423113210@gmail.com>
2023-04-01 00:04:53 +0900

    3-2

---
+++ src/chapter3/test_perceptron.py
@@ -0,0 +1,31 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from sklearn import datasets
+import numpy as np
+
+if __name__ == "__main__":
+    sc = StandardScaler()
+
+    iris = datasets.load_iris()
+
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=1, stratify=y
+    )
+
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+
+    ppn = Perceptron(eta0=0.01, random_state=1)
+    ppn.fit(X_train_std, y_train)
+
+    y_pred = ppn.predict(X_test_std)
+
+    print(f"Missclassified examples: {(y_test != y_pred).sum()}")
+    print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
+    print(f"Accuracy: {ppn.score(X_test_std, y_test)}")
