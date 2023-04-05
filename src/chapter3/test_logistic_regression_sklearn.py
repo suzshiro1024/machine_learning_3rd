acd500745687807799d1ddba14ecfc33ab205bb1
suzshiro1024 <tix131423113210@gmail.com>
2023-04-06 00:50:01 +0900

    3-3-4

---
+++ src/chapter3/test_logistic_regression_sklearn.py
@@ -0,0 +1,25 @@
+from data_gen import data_gen
+from region_plot import plot_decision_regions
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    X_train_std, X_test_std, y_train, y_test = data_gen()
+
+    lr = LogisticRegression(C=100.0, random_state=1, solver="lbfgs", multi_class="ovr")
+    lr.fit(X_train_std, y_train)
+
+    X_combined_std = np.vstack((X_train_std, X_test_std))
+    y_combined = np.hstack((y_train, y_test))
+
+    plot_decision_regions(
+        X=X_combined_std, y=y_combined, classifier=lr, test_idx=range(105, 150)
+    )
+
+    plt.xlabel("petal length [standardized]")
+    plt.ylabel("petal width [standardized]")
+    plt.legend(loc="upper left")
+    plt.tight_layout()
+
+    plt.savefig("../../figure/test_lr_sklearn.png")
---
+++ src/chapter3/test_predict_proba.py
@@ -0,0 +1,23 @@
+from data_gen import data_gen
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+if __name__ == "__main__":
+    X_train_std, X_test_std, y_train, y_test = data_gen()
+
+    lr = LogisticRegression(C=100.0, random_state=1, solver="lbfgs", multi_class="ovr")
+    lr.fit(X_train_std, y_train)
+
+    print(f"prob:\n {lr.predict_proba(X_test_std[:3, :])}")
+
+    # print(lr.predict_proba(X_test_std[:3, :]).sum(axis=1))
+
+    print(f"predict:\n {lr.predict_proba(X_test_std[:3, :]).argmax(axis=1)}")
+
+    print(f"predict:\n {lr.predict(X_test_std[:3, :])}")
+
+    # bad
+    # print(f"predict:\n {lr.predict(X_test_std[0,:])}")
+
+    # good (配列の次元がpredictで扱える二次元に自動で変更される)
+    print(f"predict:\n {lr.predict(X_test_std[0,:].reshape(1,-1))}")
