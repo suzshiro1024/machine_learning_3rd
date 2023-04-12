9717a09dd61a7d55b9cbc926f276e0e81aec55e6
suzshiro1024 <tix131423113210@gmail.com>
2023-04-13 00:07:43 +0900

    3-6-2

---
+++ src/chapter3/test_tree.py
@@ -0,0 +1,35 @@
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from region_plot import plot_decision_regions
+from sklearn.tree import DecisionTreeClassifier
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    iris = datasets.load_iris()
+
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=1, stratify=y
+    )
+
+    tree_model = DecisionTreeClassifier(criterion="gini", max_depth=4, random_state=1)
+    tree_model.fit(X_train, y_train)
+
+    X_combined = np.vstack((X_train, X_test))
+    y_combined = np.hstack((y_train, y_test))
+
+    plot_decision_regions(
+        X_combined, y_combined, classifier=tree_model, test_idx=range(105, 150)
+    )
+
+    plt.xlabel("petal length [cm]")
+    plt.ylabel("petal width [cm]")
+    plt.legend(loc="upper left")
+    plt.tight_layout()
+
+    plt.savefig("../../figure/test_tree.png")
