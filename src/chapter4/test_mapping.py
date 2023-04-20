385df7bfef8b5a53944fe16753dbcbe8b6d1573a
suzshiro1024 <tix131423113210@gmail.com>
2023-04-21 00:09:59 +0900

    4-2-3

---
+++ src/chapter4/test_mapping.py
@@ -0,0 +1,13 @@
+from category_gen import category_gen
+
+if __name__ == "__main__":
+    df = category_gen()
+
+    size_mapping = {"XL": 3, "L": 2, "M": 1}
+    inv_size_mapping = {v: k for k, v in size_mapping.items()}
+
+    df["size"] = df["size"].map(size_mapping)
+    print(f"df\n{df}")
+
+    df["size"] = df["size"].map(inv_size_mapping)
+    print(f"df\n{df}")
