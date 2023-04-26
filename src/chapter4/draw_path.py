735fc2efb3050fa4219bbb2dbc92e9b03b0eb0e2
suzshiro1024 <tix131423113210@gmail.com>
2023-04-27 00:35:06 +0900

    4-5-3

---
+++ src/chapter4/draw_path.py
@@ -0,0 +1,69 @@
+from wine_gen import wine_data_gen
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+if __name__ == "__main__":
+    df_wine = pd.read_csv("../../data/wine.data", header=None)
+
+    X_train, X_test, y_train, y_test = wine_data_gen()
+
+    stdsc = StandardScaler()
+    X_train_std = stdsc.fit_transform(X_train)
+    X_test_std = stdsc.transform(X_test)
+
+    fig = plt.figure()
+    ax = plt.subplot(111)
+
+    colors = [
+        "blue",
+        "green",
+        "red",
+        "cyan",
+        "magenta",
+        "yellow",
+        "black",
+        "pink",
+        "lightgreen",
+        "lightblue",
+        "gray",
+        "indigo",
+        "orange",
+    ]
+
+    weights, params = [], []
+
+    for c in np.arange(-4.0, 6.0):
+        lr = LogisticRegression(
+            penalty="l1",
+            C=10.0 ** c,
+            solver="liblinear",
+            multi_class="ovr",
+            random_state=0,
+        )
+
+        lr.fit(X_train_std, y_train)
+        weights.append(lr.coef_[1])
+        params.append(10 ** c)
+
+    weights = np.array(weights)
+
+    for column, color in zip(range(weights.shape[1]), colors):
+        plt.plot(
+            params, weights[:, column], label=df_wine.columns[column + 1], color=color
+        )
+
+    plt.axhline(0, color="black", linestyle="--", linewidth=3)
+    plt.xlim([10 ** (-5), 10 ** 5])
+
+    plt.ylabel("weight coefficient")
+    plt.xlabel("C")
+
+    plt.xscale("log")
+    plt.legend(loc="upper left")
+    ax.legend(loc="upper center", bbox_to_anchor=(1.38, 1.03), ncol=1, fancybox=True)
+
+    plt.savefig("../../figure/L1_path.png")
---
+++ src/chapter4/test_L1.py
@@ -0,0 +1,17 @@
+from wine_gen import wine_data_gen
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = wine_data_gen()
+
+    stdsc = StandardScaler()
+    X_train_std = stdsc.fit_transform(X_train)
+    X_test_std = stdsc.transform(X_test)
+
+    lr = LogisticRegression(penalty="l1", C=1.0, solver="liblinear", multi_class="ovr")
+    lr.fit(X_train_std, y_train)
+
+    print(f"Training Accuracy: {lr.score(X_train_std, y_train)}")
+    print(f"Test Accuracy: {lr.score(X_test_std, y_test)}")
